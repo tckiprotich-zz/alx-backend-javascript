e219bfd8fd8914f648ee342d65489c23eba6f5d8
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-26 20:05:47 +0000

    This function takes an iterable object

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -3,12 +3,12 @@
  * @param {Object} report - The report object containing all employees.
  * @yields {Object} The next employee object in the report.
  */
-export default function createIteratorObject(report) {
-  return (function* _() {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department) {
-        yield employee;
-      }
+export default function iterateThroughObject(reportWithIterator) {
+    const employees = [];
+  
+    for (const employee of reportWithIterator) {
+      employees.push(employee);
     }
-  }());
-}
+  
+    return employees.join(' | ');
+  }
