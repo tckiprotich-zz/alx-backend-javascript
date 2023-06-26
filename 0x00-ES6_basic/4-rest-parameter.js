a9d1654162c6664436505bfa709c676931cda3f5
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-26 18:36:14 +0000

    Calculates the number of arguments passed to the function.

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,8 @@
+/**
+ * Calculates the number of arguments passed to the function.
+ * @param {...*} args - The arguments to count.
+ * @returns {number} The number of arguments passed to the function.
+ */
+export default function returnHowManyArguments(...args) {
+    return args.length;}
+
