a715ca21fe5a0f0ea80a38f9b16dbbf93f25bb05
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-26 18:44:47 +0000

    Returns an object containing the income, gdp, and capita values passed as arguments.

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,17 @@
+/**
+ * Returns an object containing the income, gdp, and capita values passed as arguments.
+ *
+ * @param {number} income - The income value to be included in the budget object.
+ * @param {number} gdp - The gdp value to be included in the budget object.
+ * @param {number} capita - The capita value to be included in the budget object.
+ * @returns {object} - An object containing the income, gdp, and capita values.
+ */
+export default function getBudgetObject(income, gdp, capita) {
+    const budget = {
+        income,
+        gdp,
+        capita,
+    };
+
+    return budget;}
+
