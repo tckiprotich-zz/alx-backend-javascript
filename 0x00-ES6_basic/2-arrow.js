561b119d5e1dbf6794c3f6ca4a0fe20c5921b156
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-26 18:23:41 +0000

    Refactor the getNeighborhoodsList function to explicitly reference San Francisco neighborhoods, and remove unrelated package-lock lines.

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,12 +1,12 @@
 /**
- * Defines a function that returns a list of neighborhoods in San Francisco and allows adding new neighborhoods to the list.
- * @returns {function} A function that returns an array of neighborhoods in San Francisco and allows adding new neighborhoods to the list.
+ * Defines a function that returns a list of San Francisco neighborhoods and allows adding new neighborhoods to the list.
+ * @returns {function} A function that returns an array of San Francisco neighborhoods and allows adding new neighborhoods to the list.
  */
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    this.addNeighborhood = (newNeighborhood) => {
-      this.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return this.sanFranciscoNeighborhoods;
-    };
-  }
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
