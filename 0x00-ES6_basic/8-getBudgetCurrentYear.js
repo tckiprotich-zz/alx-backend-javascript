140a8191f580bad493a23c9789e0ab6261b4942e
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-26 18:46:53 +0000

    Returns a budget object for the current year.

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,25 @@
+/**
+ * Returns the current year.
+ * @returns {number} The current year.
+ */
+function getCurrentYear() {
+    const date = new Date();
+    return date.getFullYear();
+}
+
+/**
+ * Returns a budget object for the current year.
+ * @param {number} income - The income for the current year.
+ * @param {number} gdp - The GDP for the current year.
+ * @param {number} capita - The capita for the current year.
+ * @returns {object} A budget object for the current year.
+ */
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+    const budget = {
+        [`income-${getCurrentYear()}`]: income,
+        [`gdp-${getCurrentYear()}`]: gdp,
+        [`capita-${getCurrentYear()}`]: capita,
+    };
+
+    return budget;}
+
