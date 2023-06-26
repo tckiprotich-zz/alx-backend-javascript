0c793f291cd9a3c32bf092e4326557469c1f7032
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-26 19:41:24 +0000

    Returns a full budget object with income, gdp, capita,

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,24 +1,24 @@
 /**
- * Returns a full budget object with income, gdp, capita, 
+ * Returns a full budget object with income, gdp, capita,
  * and additional methods to get income in dollars and euros.
  * @param {number} income - The income value.
  * @param {number} gdp - The gdp value.
  * @param {number} capita - The capita value.
  * @returns {object} - The full budget object.
  */
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
-    const budget = getBudgetObject(income, gdp, capita);
-    const fullBudget = {
-        ...budget,
-        getIncomeInDollars(income) {
-            return `$${income}`;
-        },
-        getIncomeInEuros(income) {
-            return `${income} euros`;
-        },
-    };
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
 
-    return fullBudget;
+  return fullBudget;
 }
