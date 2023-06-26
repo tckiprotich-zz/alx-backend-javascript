bc5f1fde9289d46596689443e1226c7f4d842976
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-26 18:38:48 +0000

    Concatenates two arrays and a string using the spread operator.

---
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -0,0 +1,10 @@
+/**
+ * Concatenates two arrays and a string using the spread operator.
+ * @param {Array} array1 - The first array to concatenate.
+ * @param {Array} array2 - The second array to concatenate.
+ * @param {string} string - The string to concatenate.
+ * @returns {Array} - The concatenated array.
+ */
+export default function concatArrays(array1, array2, string) {
+    return [...array1, ...array2, ...string];}
+
