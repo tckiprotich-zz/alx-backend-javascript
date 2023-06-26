073fcb5bc0e8202084cc3c735b5d0c06a71093f0
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-26 13:49:11 +0000

    initial setup

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,26 @@
+/**
+ * This module exports three functions:
+ * - taskFirst: returns a string "I prefer const when I can." using the const keyword to declare a variable.
+ * - getLast: returns a string " is okay".
+ * - taskNext: uses the let keyword to declare a variable and returns a string "But sometimes let is okay".
+ * 
+ * To declare a variable in ES6, we use one of two keywords: const or let.
+ * const is short for constant. It's used for variables that we don't expect to change.
+ * let is used for variables that we do expect to change.
+ */
+
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+}
+
+export function getLast() {
+    return ' is okay';
+}
+
+export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+
+    return combination;
+}
