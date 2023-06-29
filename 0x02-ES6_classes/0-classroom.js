594c1463b1e482d2b7ca03043e3fb7c6241760d7
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-29 08:11:47 +0000

    Represents a classroom with a maximum number of students.

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a classroom with a maximum number of students.
+ */
+export default class ClassRoom {
+    /**
+     * Creates a new instance of the ClassRoom class.
+     * @param {number} maxStudentsSize - The maximum number of students
+     *  allowed in the classroom.
+     */
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;
+    }}
+
