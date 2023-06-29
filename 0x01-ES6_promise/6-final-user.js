d0df536ac106b321b90b5cb83e6bb31c394f6562
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-29 07:18:34 +0000

    This module exports three functions

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,22 @@
+/**
+ * Asynchronously handles the signup of a user's profile by
+ *  calling the signUpUser and uploadPhoto functions.
+ * @param {string} firstName - The first name of the user.
+ * @param {string} lastName - The last name of the user.
+ * @param {string} fileName - The name of the file to upload.
+ * @returns {Promise} A promise that resolves to an array of
+ *  objects containing the status and value of each promise.
+ */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
+}
