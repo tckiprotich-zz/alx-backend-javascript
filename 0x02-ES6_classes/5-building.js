7e8fe9be8c05d5cf2b67ee9cfa2391ca33c4688f
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-29 14:29:54 +0000

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+/**
+ * Represents a building with a certain square footage.
+ * @class
+ */
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
