d37bef3200d1142b1df86bacd01743fa3ecdfeb3
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-29 14:06:50 +0000

    0x02-ES6_classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,12 +2,12 @@
  * Represents a classroom with a maximum number of students.
  */
 export default class ClassRoom {
-    /**
+  /**
      * Creates a new instance of the ClassRoom class.
      * @param {number} maxStudentsSize - The maximum number of students
      *  allowed in the classroom.
      */
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-    }}
-
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
