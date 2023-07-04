5bf3a8c2cac7b21a7b275357f837d850c3ac7196
COLLINS  TONUI <tckiprotich@gmail.com>
2023-07-04 19:24:01 +0000

    6-set.js

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Returns a new Set object from an array.
+ *
+ * @param {Array} array - The array to be converted to a Set object.
+ * @returns {Set} A new Set object containing the elements of the input array.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
