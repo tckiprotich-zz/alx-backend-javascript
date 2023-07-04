2156071a850d5698471c383cca59fc493140d058
COLLINS  TONUI <tckiprotich@gmail.com>
2023-07-04 16:53:19 +0000

    Returns an array of student objects with their id, first name, and location.

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * Returns an array of student objects with their id, first name, and location.
+ * @returns {Array} Array of student objects
+ */
+export default function getListStudents() {
+    return [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+}
