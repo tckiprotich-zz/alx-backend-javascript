b0af3c041c3a334d8d82c879341ea16c6f4a4989
tckiprotich <tckiprotich@gmail.com>
2023-08-22 12:04:32 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,10 @@
-// message to be printed
-const message = "Welcome to Holberton School, what is your name? \n";
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// print message to terminal
-process.stdout.write(message);
-
-// read user input
-process.stdin.on('readable', () => {
-    const name = process.stdin.read();
-    if (name !== null) {
-        console.log(`Your name is: ${name}`);
-    }
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
-// closing the process
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
